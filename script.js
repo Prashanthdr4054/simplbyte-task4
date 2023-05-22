34f2cc1e04b680ea2250dc502b35a170453257c9
Prashanthdr4054 <120126150+Prashanthdr4054@users.noreply.github.com>
2023-05-22 21:06:26 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Weather App</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <div class="app-wrap">
+    <header>
+      <input type="text" autocomplete="off" class="search-box" placeholder="Search for a city..." />
+    </header>
+    <main>
+      <section class="location">
+        <div class="city">Gwalior, IN</div>
+        <div class="date">Tuesday 2 May 2023</div>
+      </section>
+      <div class="current">
+        <div class="temp">29<span>°c</span></div>
+        <div class="weather">Sunny</div>
+        <div class="hi-low">40°c / 19°c</div>
+      </div>
+    </main>
+  </div>
+  <script src="scripts.js"></script>
+</body>
+</html>
